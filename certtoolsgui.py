fcbb2012da76c14a2cf535472a57600087c685bd
TJ <tjmessinger@gmail.com>
2020-06-03 08:36:08 -0700

    Fixed paste functionality, added Json output in gui, general asthetic changes.

--- certtoolsgui.py
+++ certtoolsgui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font as font
 from certtools import x509Cert
 
 
@@ -6,18 +7,26 @@ class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        self.master.title("PyCertTools")
+        self.master.iconbitmap("pycerttools.ico")
         self.pack()
         self.create_widgets()
         self.create_context_menu()
         self.create_commands()
 
     def create_widgets(self):
-        self.lbl_input = tk.Label(master=self, text='Cert Text')
+        btn_font = font.Font(size=14)
+        lbl_font = font.Font(size=20, weight="bold")
+        self.lbl_input = tk.Label(master=self, text='Cert Text', font=lbl_font)
         self.lbl_input.pack()
         self.txt_input = tk.Text(master=self)
         self.txt_input.pack()
-        self.btn_input = tk.Button(master=self, text='parse')
-        self.btn_input.pack()
+        self.btn_input = tk.Button(master=self, text='parse', font=btn_font)
+        self.btn_input.pack(side=tk.LEFT, padx=50, pady=10)
+        self.btn_json = tk.Button(master=self, text='json', font=btn_font)
+        self.btn_json.pack(side=tk.LEFT, padx=20, pady=10)
+        self.btn_clear = tk.Button(master=self, text='clear', font=btn_font)
+        self.btn_clear.pack(side=tk.RIGHT, padx=50, pady=10)
 
     def create_context_menu(self):
         self.popup_menu = tk.Menu(self, tearoff=0)
@@ -31,15 +40,22 @@ class Application(tk.Frame):
                                     command=self.select_all)
 
     def create_commands(self):
+        self.btn_clear.bind('<Button-1>', self.clear)
         self.btn_input.bind('<Button-1>', self.parse_cert)
+        self.btn_json.bind('<Button-1>', self.json_cert)
         self.txt_input.bind("<Button-3>", self.popup)
         self.master.bind("<Control-a>", self.select_all)
+        self.master.bind("<Return>", self.parse_cert)
 
     def popup(self, event):
         try:
             self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
         finally:
             self.popup_menu.grab_release()
+
+    def clear(self, event=None):
+        self.select_all()
+        self.txt_input.delete("1.0", tk.END)
 
     def copy(self):
         self.master.clipboard_clear()
@@ -57,7 +73,10 @@ class Application(tk.Frame):
 
     def paste(self):
         text = self.master.selection_get(selection='CLIPBOARD')
-        self.txt_input.delete("sel.first", "sel.last")
+        try:
+            self.txt_input.delete("sel.first", "sel.last")
+        except tk.TclError:
+            pass
         self.txt_input.insert('insert', text)
 
     def parse_cert(self, event):
@@ -67,6 +86,11 @@ class Application(tk.Frame):
         self.txt_input.delete(1.0, tk.END)
         self.txt_input.insert(1.0, cert.print())
 
+    def json_cert(self, even):
+        data = self.txt_input.get(1.0, tk.END)
+        cert = x509Cert(data)
+        self.txt_input.delete(1.0, tk.END)
+        self.txt_input.insert(1.0, cert.to_json())
 
 if __name__ == '__main__':
     root = tk.Tk()
